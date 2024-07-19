b4303eec4c9dd16e7910d93c0fcc6c1fffa69b42
Dhianahdi <mongi.nahdi@gmail.com>
2024-07-19 17:00:46 +0100

    2355

--- Back/index.js
+++ Back/index.js
@@ -77,7 +77,12 @@ app.use((req, res, next) => {
   )
   next()
 })
+const corsOptions = {
+  origin: 'https://employee-work-mange.vercel.app',
+  optionsSuccessStatus: 200 // Pour les navigateurs plus anciens
+};
 
+app.use(cors(corsOptions));
 // Configure multer for image uploads
 const imageStorage = multer.diskStorage({
   destination: function (req, file, cb) {
